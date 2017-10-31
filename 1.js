d45866945b06f986f8f390b9571b8f95bd2d4d76
unknown <ahieuiv@gmail.com>
2017-10-31 15:24:08 +0700

    tao ra file moi

---
+++ 1.js
@@ -0,0 +1,27 @@
+﻿$(function(){
+	//Viết cho nút
+	$('nav ul li a').click(function(event) {
+		//console.log('hello');
+
+		//Phát hiện data của nút được click
+		var danhmuc = $(this).data('class');		
+		//console.log(danhmuc);
+
+		var ten = $(this).text();
+
+		$('section h1').text(ten)
+		
+		$('.noidung .content ul li').each(function() {
+			if($(this).hasClass(danhmuc))
+			{
+				$(this).fadeIn();
+			}	
+			else
+			{
+				$(this).fadeOut();
+			}
+		});
+		return false;
+	});	
+});  
+ 
